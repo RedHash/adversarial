c78acbdd8ed1a58c15e27ec34bb1a2932aa529ab
Filippov <filippovis@yahoo.com>
2020-06-11 02:22:33 +0300

    Bash scripts for easy experiments starting

--- run_eval.sh
+++ run_eval.sh
@@ -1,1 +1,1 @@
-python -W ignore -m main --type eval --wanted_words yes  --dev_every 1 --n_labels 12 --model res8-narrow --input_file model/res8_narrow.pt --no_cuda --data_folder data/speech_dataset
+python -W ignore -m main --type eval --wanted_words yes  --dev_every 1 --n_labels 12 --model res8-narrow --input_file honk/model/res8_narrow.pt --no_cuda --data_folder honk/data/speech_dataset
---
+++ run_train.sh
@@ -0,0 +1,1 @@
+python -W ignore -m main --type train --wanted_words yes --dev_every 1 --n_labels 12 --n_epochs 10 --model res8-narrow --input_file honk/model/res8_narrow.pt --no_cuda --data_folder honk/data/speech_dataset --batch_size 512
